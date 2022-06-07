61c7c838df6ae3cadd6c74c29a2ca351d333528f
Himanshu <himukumar005@gmail.com>
2022-06-07 21:56:25 +0530

    modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
-import Navbar from "./components/Navbar";
+import Header from "./Header";
+import Navbar from "./Navbar";
 
 function App() {
   return (
     <>
       {/* <h1>web payment app</h1> */}
       <Navbar />
+      <Header />
     </>
   );
 }

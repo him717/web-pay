6dc00e991e0e681391b1a978aa1d61df18075c43
Himanshu <himukumar005@gmail.com>
2022-06-25 19:21:49 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Home from "./Home";
-import { Route, Routes } from "react-router";
+import { Route, Routes } from "react-router-dom";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Service from "./pages/Service";
